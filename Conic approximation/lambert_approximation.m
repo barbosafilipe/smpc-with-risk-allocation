bd465fb0520b6c397b567c9606f40ac28e97bc14
barbosafilipe <filipe.mbarbosa@outlook.com>
2026-01-08 21:10:26 +0100

    Clean up and organization in the algorithm to obtain the approximation

--- Conic approximation/lambert_approximation.m
+++ Conic approximation/lambert_approximation.m
@@ -2,11 +2,11 @@ addpath(genpath('your_path_to_YALMIP'))
 addpath(genpath('your_path_to_mosek'))
 yalmip('clear')
 
-%% Using Lambert
+%% Using the Lambert-W function as an exponential cone approximation to the probit function
 
-x0 = 0.0001;
-x = x0:0.0001:0.5;
+x0 = 0.0001; x = x0:0.0001:0.5; % Define the region where we want to approxmate
 
+% Initial guesses of the parameters
 a0 = 0.70;
 b0 = 600;
 c0 = -0.75;
@@ -17,23 +17,24 @@ z = sdpvar(length(x),1);
 warmstart([a b c k],[a0 b0 c0 k0]);
 
 obj = 0;
-Con = -a*lambertw(b*x(1))+k >= norminv(1-x(1));
+con = -a*lambertw(b*x(1))+k >= norminv(1-x(1));
 
+% The curve fitting problem is formulated as a non-negative least-squares regression
 for i = 1:100:length(x)
     fi = -a*lambertw(b*x(i)) + k + c*x(i);
-    yi = norminv(1-x(i));    
-    Con = [Con, z(i) == (fi-yi), z(i) >= 0];
+    yi = norminv(1-x(i));
+    con = [con, z(i) == (fi-yi), z(i) >= 0];
     obj = obj + (1 + 10000*(i==1))*(z(i))^2;
-    % assign(z(i), value(fi)-yi);
 end
-optimize(Con,obj,sdpsettings('usex0',1,'solver','ipopt'));
+optimize(con,obj,sdpsettings('usex0',1,'solver','ipopt'));
 
 %%
-% a = value(a);
-% b=value(b);
-% k=value(k);
-% c = value(c);
+a = value(a);
+b=value(b);
+k=value(k);
+c = value(c);
 
+% The values used on the paper:
 a = 0.499492956059166;
 b = 8.082867432374761e+03;
 k = 3.965651977413067;
@@ -42,26 +43,18 @@ c = -1.475743096725997;
 x0 = 0.00001;
 x = x0:0.0001:1;
 lamb=@(x)(-a*lambertw(b*x)+k+c*x);
-xn = x0:0.0001:1;
+
 %% Plots
 
-red = [0.8941 0.0235 0.0235];
-gray = [0.5 0.5 0.5];
-yellow = [0.2667,0.4471,0.7098];
-purple = [0.8275,0.2627,0.3059];
-darkGreen = [0.1647,0.6706,0.3804];
-blue = [0 0 1];
-cyan = [0 1 1];
-co1 = [0.9290 0.6940 0.1250];
-co2 = [0.8500 0.3250 0.0980];
-co3 = '#F09A11';
-co4 = '#188AF0';
+co1 = [0.8500 0.3250 0.0980];
+co2 = '#F09A11';
+co3 = '#188AF0';
 
-%%
+%% Plot the probit function and the obtained approximations
 figure('Position', [200, 100, 1600, 900]);hold on;
 hold on
-plot(x,norminv(1-x),'color',co3,'LineWidth',4)
-plot(x,lamb(x),':','color',co4,'LineWidth',5)
+plot(x,norminv(1-x),'color',co2,'LineWidth',4)
+plot(x,lamb(x),':','color',co3,'LineWidth',5)
 axis([-0.01 1.01 -4.5 4])
 h1 = gca;
 h1.XAxis.FontSize = 20;
@@ -69,12 +62,11 @@ h1.YAxis.FontSize = 20;
 xlabel('$\gamma$','Interpreter', 'latex','FontSize',55)
 legend('$\Phi^{-1}(1-\gamma)$','$\Psi(\gamma)$','Interpreter','latex','FontSize',35)
 legend boxoff;
-% drawnow;
-% exportgraphics(gca,'C:\Users\filma90\Documents\Texts\writing\Journal\IJC\fig\approximation.eps')
-%%
+
+%% Plot the logarithm error
 figure('Position', [200, 100, 1600, 900]);hold on;
 hold on
-plot(x,log(lamb(x)-norminv(1-x)),'color',co2,'LineWidth',4)
+plot(x,log(lamb(x)-norminv(1-x)),'color',co1,'LineWidth',4)
 axis([-0.01 0.5 -22 0])
 h2 = gca;
 h2.XAxis.FontSize = 20;
@@ -82,24 +74,3 @@ h2.YAxis.FontSize = 20;
 xlabel('$\gamma$','Interpreter', 'latex','FontSize',55)
 legend('$\log e(\gamma)$','Interpreter','latex','FontSize',35,'Location','NorthEast')
 legend boxoff;
-% drawnow;
-% exportgraphics(gca,'C:\Users\filma90\Documents\Texts\writing\Journal\IJC\fig\error.eps')
-
-
-%%
-
-figure('Position', [200, 100, 1600, 900]);hold on;
-hold on
-plot(xn,norminv(1-xn),'color',co3,'LineWidth',4)
-plot(x,lamb(x),':','color',co4,'LineWidth',5)
-axis([-0.01 3.01 -4.5 4])
-grid on
-h1 = gca;
-h1.XAxis.FontSize = 20;
-h1.YAxis.FontSize = 20;
-h1.GridLineStyle = "-.";
-xlabel('$\zeta$','Interpreter', 'latex','FontSize',35)
-legend('$\Phi^{-1}(1-\zeta)$','$\Psi(\zeta)$','Interpreter','latex','FontSize',35)
-legend boxoff;
-% drawnow;
-% exportgraphics(gca,'C:\Users\filma90\Documents\Texts\writing\Journal\TCST\fig\approximation-extended.eps')
